c36d316cde98aa0b111e9229844b68af579c44a2
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2023-07-17 19:37:18 -0300

    data components

--- aplicacao/src/components/PrimeiroComponente.vue
+++ aplicacao/src/components/PrimeiroComponente.vue
@@ -1,9 +1,18 @@
 <template>
-      <h1>Olá Vue!</h1>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>Meu nome é: {{ name }} e trabalho como {{ profissao }}</p>
+    </div>
 </template>
 
 <script>
     export default {
-        name: 'PrimeiroComponente'
+        name: 'PrimeiroComponente',
+        data() {
+            return {
+                name: 'Guilherme',
+                profissao: 'Dev'
+            }
+        }
     }
 </script>
