ed851378d21140f637fbdc95f54d941e973502cd
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2023-07-17 21:20:17 -0300

    hierarchy components

--- aplicacao/src/App.vue
+++ aplicacao/src/App.vue
@@ -2,17 +2,23 @@
   <h1>Olá mundo!</h1>
   <PrimeiroComponente />
   <LifeCycle />
+  <Pessoa />
+  <Form />
 </template>
 
 <script>
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
 import LifeCycle from './components/LifeCycle.vue';
+import Pessoa from './components/Pessoa.vue';
+import Form from './components/Form.vue';
 
   export default {
     name: 'app',
     components: {
       PrimeiroComponente,
-      LifeCycle
+      LifeCycle,
+      Pessoa,
+      Form
     }
   }
 </script>
---
+++ aplicacao/src/components/Pessoa.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>Essa é a descrição da pessoa: {{ name }}</h2>
+        <Info />
+    </div>
+</template>
+
+
+<script>
+    import Info from './Info'
+
+    export default {
+        name: 'Pessoa',
+        components: {
+            Info
+        },
+        data() {
+            return {
+                name: 'Guilherme'
+            }
+        }
+    }
+</script>
